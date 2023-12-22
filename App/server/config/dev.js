5a8c0c2223e7fc5858d638dcf8f0978989712620
jeongsaeyeong <ooooo0516@naver.com>
2023-12-22 11:03:14 +0900

    m

---
+++ App/server/config/dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: "mongodb+srv://jeongsaeyeong:Angel0516!@cluster0.0mh2vfb.mongodb.net/reactBlog?retryWrites=true&w=majority",
+    access_key: "HpNWFRM1HLFREymZVJN9",
+    secret_key: "leM06k4JAhq5tmagHLj5S6C264bM0bLgmNy1qapz"
+}
---
+++ App/server/config/production.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: process.env.MONGO_URI,
+    access_key: proceess.env.S3_KEY,
+    secret_key: proceess.env.S3_SECRET,
+}
